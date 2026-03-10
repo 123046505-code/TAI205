ad2a29b112cc44bb8f27bfcfbbfdd2340d5c3503
123046505-code <123046505@upq.edu.mx>
2026-03-09 20:30:31 -0600

    2commits

--- Examen2p/app/main.py
+++ Examen2p/app/main.py
@@ -1,9 +1,8 @@
 
 from fastapi import FastAPI,status,HTTPException, Depends
-
-from fastapi import FastAPI
-from typing import Optional
-import asyncio
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+import secrets
+from pydantic import BaseModel,Field
 
 app= FastAPI()
 app= FastAPI(
@@ -12,14 +11,37 @@ app= FastAPI(
     version='1.0.0')
 
 Reservas=[
-    {"id":"1","huesped:":"Diana", "fecha entrada":"25/1/26", "fecha salida":"27/1/26", "tipo abitacion":"sencilla"},
-    {"id":"2","huesped:":"Gael","fecha entrada":"25/1/26", "fecha salida":"27/1/26", "tipo abitacion":"suite"},
-    {"id":"3","huesped:":"eliseo", "fecha entrada":"25/1/26", "fecha salida":"27/1/26", "tipo abitacion":"doble"},
+    {"id":"1","huesped:":"Diana", "fecha entrada":"25/1/26", "fecha salida":"27/1/26", "tipo habitacion":"sencilla"},
+    {"id":"2","huesped:":"Gael","fecha entrada":"25/1/26", "fecha salida":"27/1/26", "tipo habitacion":"suite"},
+    {"id":"3","huesped:":"eliseo", "fecha entrada":"25/1/26", "fecha salida":"27/1/26", "tipo habitacion":"doble"},
 ]
 
+#SEGURIDAD
 
-@app.post("/v1/usuarios/", tags=['crear reserva'])
-async def crear_reserva(Reservas:crear_reserva):
+class crear_usuario(BaseModel):
+
+    id: int = Field( ... ,gt=0, description="Identificador de usuario")
+
+seguridad = HTTPBasic()
+
+def verificar_peticion(credenciales:HTTPBasicCredentials=Depends(seguridad)):
+
+    userAuth= secrets.compare_digest(credenciales.username,"hotel")
+
+    passAuth= secrets.compare_digest(credenciales.password,"r2026")
+
+    if not (userAuth and passAuth ):
+        raise HTTPException(
+            status_code= status.HTTP_401_UNAUTHORIZED,
+            detail= "credenciales no autorizadas"
+        )
+    return credenciales.username
+
+
+
+#CREAR RESERVA
+@app.post("/v1/Reserva/", tags=['crear reserva'])
+async def crear_reserva(id: int, userAuth:str=Depends(verificar_peticion)):
 
     for usr in Reservas:
         if usr["id"] == Reservas.id:
@@ -31,8 +53,48 @@ async def crear_reserva(Reservas:crear_reserva):
     return{
         "mensaje":"reserva agregado correctamente",
         "status":"200",
-        "usuario":usuario
+        "reserva":Reservas
     }
+
+# ELIMINAR/CANCELAR RESERVA
+@app.delete("/v1/Reservas/{id}", tags=['eliminar reserva'])
+async def eliminar_Reserva(id: int, userAuth:str=Depends(verificar_peticion)):
+
+    for usr in Reservas:
+        if usr["id"] == id:
+            Reservas.remove(usr)
+            return {
+                "mensaje": f"Reserva eliminada correctamente por {userAuth}",
+                "status": "200",
+                "reserva_eliminado": usr
+            }
+            
+    raise HTTPException(status_code=404, detail="Reserva no encontrada")
+
+
+# MOSTRAR RESERVAS
+@app.get("/v1/Reservas/", tags=['Listar reservas'])
+async def cosultaT():
+
+    return{
+        "status":"200",
+        "total": len(Reservas),
+        "data":Reservas
+    }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
